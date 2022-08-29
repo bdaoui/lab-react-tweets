8d18a456f27c390279413b3ee40c5e81814f7aff
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-29 21:21:27 +0200

    image functioning

---
+++ src/components/User.js
@@ -0,0 +1,8 @@
+// function User(props){
+    
+//     <span className="user">
+//     <span className="name"> {tweet.name}</span>
+//     <span className="handle"> {tweet.handle}</span>
+//   </span>
+
+// }
